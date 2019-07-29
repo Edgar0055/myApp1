2dfeada51a0fb579cdda22cd734aa7db8d089971
Edgar0055 <e.rostomian13@gmail.com>
2019-07-29 16:06:59 +0300

    func_for_remove_Category

--- controllers/category.js
+++ controllers/category.js
@@ -1,5 +1,6 @@
 const Category = require('../models/Category')
 const errorHandler = require('../utils/errorHandler')
+const Position = require('../models/Position')
 
 module.exports.getAll = async function(req, res) {
     try {
@@ -19,8 +20,16 @@ module.exports.getById = async function(req, res) {
     }
 }
 
-module.exports.remove = function(req, res) {
-
+module.exports.remove = async function(req, res) {
+    try {
+        await Category.remove({ _id: req.params.id })
+        await Position.remove({ category: req.params.id })
+        res.status(200).json({
+            message: "Category was deleted"
+        })
+    } catch (e) {
+        errorHandler(res, e)
+    }
 }
 
 module.exports.create = function(req, res) {
